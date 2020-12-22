80d762211966459c9b278d97f9eb95c900ec44cf
Jannik <jannik.haberbosch@gmail.com>
2020-12-22 23:47:41 +0100

    Working on the main form.

---
+++ Hagyma/Hagyma/PresenterNewProject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hagyma
+{
+    class PresenterNewProject
+    {
+        PresenterMain parentPresenter;
+
+
+        public PresenterNewProject(PresenterMain _parentPresenter)
+        {
+            parentPresenter = _parentPresenter;
+        }
+    }
+}
