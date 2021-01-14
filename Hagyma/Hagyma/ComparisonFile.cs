90ee768b999bc5550105d7c9ac3df2627b95a192
Jannik <jannik.haberbosch@gmail.com>
2021-01-14 02:15:21 +0100

    FTP pre-stuff.

--- Hagyma/Hagyma/ComparisonFile.cs
+++ Hagyma/Hagyma/ComparisonFile.cs
@@ -10,8 +10,21 @@ namespace Hagyma
         XmlReader xmlReader;
         XmlWriter xmlWriter;
 
-        public ComparisonFile()
+        string filePath;
+
+        public ComparisonFile(
+            string _filePath)
         {
+            // XmlReader -> Read file and put contents in a dictionary.
+            // Compare dictionary files with files that exist or don't exist yet.
+            // Add new dictionary entries for files that don't exist; overwrite dictionary entries for files that exist.
+            // Delete dictionary entries of files that don't exist anymore.
+            // Write back the file using XmlWriter.
+        }
+
+        protected void init()
+        {
+
         }
     }
 }
