cdbec2eed065c5d3394e673a48efe2a045adb58b
Jannik <jannik.haberbosch@gmail.com>
2021-01-23 16:43:23 +0100

    new release 20210123

--- Hagyma/Hagyma/Constants.cs
+++ Hagyma/Hagyma/Constants.cs
@@ -10,7 +10,7 @@ namespace Hagyma
     public class Constants
     {
         public const string name = "Hagyma";
-        public const string release = "20210117";
+        public const string release = "20210123";
         public const string database_dir = "database"; // Obsolete.
         public const string database_file = "db.sqlite";
 
